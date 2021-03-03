de0d8facfceb9cdbad85b40d5edc63aabfa91a78
cartman1978 <cartman_7802@yahoo.ie>
2021-03-03 11:42:35 +0000

    added eventListener mouse move with event for x position

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const slider = document.querySelector(".slider");
-const before = document.querySelector(".before");
-const after = document.querySelector(".after");
+const before = document.querySelector(".img-container-before");
+const after = document.querySelector(".img-container-after");
 const container = document.querySelector(".container");
 
-const drag = () => {
-
+const drag = (e) => {
+  let xPos = e.layerX;
+  before.style.width = xPos + 'px';
+  
 }
+
+container.addEventListener('mousemove', drag);
