6e16afd6356de127785439733eff69a71bb11ccd
cartman1978 <cartman_7802@yahoo.ie>
2021-03-03 11:28:00 +0000

    added querySelector to app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+const slider = document.querySelector(".slider");
+const before = document.querySelector(".before");
+const after = document.querySelector(".after");
+const container = document.querySelector(".container");
+
+const drag = () => {
+
+}
